c0b51b53c5c3db0a6558589fe743415941763236
raul-bss <codebyraul@gmail.com>
2024-03-30 18:14:55 -0300

    DATABASE H2 JPA

--- src/main/java/com/example/WebService/Spring/Entites/User.java
+++ src/main/java/com/example/WebService/Spring/Entites/User.java
@@ -3,10 +3,20 @@ package com.example.WebService.Spring.Entites;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="table_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long index;
 	
 	private String name;
@@ -84,12 +94,6 @@ public class User implements Serializable{
 		return Objects.equals(index, other.index);
 	}
 
-	@Override
-	public String toString() {
-		return "User [index=" + index + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password="
-				+ password + "]";
-	}
-	
 	
 	
 	
