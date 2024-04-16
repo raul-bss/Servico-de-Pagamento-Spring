cfbef0f99bfa5a09a903317dd4649f74566044fb
raul-bss <codebyraul@gmail.com>
2024-04-15 23:17:14 -0300

    Payment

--- src/main/java/com/example/WebService/Spring/Config/TestConfig.java
+++ src/main/java/com/example/WebService/Spring/Config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.WebService.Spring.Entites.Category;
 import com.example.WebService.Spring.Entites.Order;
 import com.example.WebService.Spring.Entites.OrderItem;
+import com.example.WebService.Spring.Entites.Payment;
 import com.example.WebService.Spring.Entites.Product;
 import com.example.WebService.Spring.Entites.User;
 import com.example.WebService.Spring.Entites.Enums.OrderStatus;
@@ -88,7 +89,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p4, 2, p4.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
-		 
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 	}
 
 }
